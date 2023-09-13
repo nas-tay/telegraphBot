e221cbac8e2819c865631f1a33db0914367e644e
Anastasia Karzanova <anastasiakarzanova@Anastasias-MacBook-Pro.local>
2023-09-13 16:07:50 +0600

    removed build

--- build/src/domains/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Config = void 0;
-exports.Config = {
-    mainMongoUrl: "mongodb+srv://anastasia:123456789mongo@cluster0.8tssfhh.mongodb.net/KrishaKZ?retryWrites=true&w=majority",
-    bot: "6568336244:AAFVemkNUwGjWoORq5V-3pCbPPCHIOsoRj8",
-    redis: "redis://127.0.0.1:6379",
-};
-//# sourceMappingURL=index.js.map
